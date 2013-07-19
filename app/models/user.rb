50e2792c5a6c76add8ad3301b65b72c4cadd0df2
root <root@Ruby-Ubuntu-Server.(none)>
2013-07-19 09:46:36 +0800

    webQQ模拟包

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class User < ActiveRecord::Base 
     attr_accessible :description, :email, :encrypted_passwd, :group_id, :group_name, :phone, :qq, :username
+    has_one :menulist
     acts_as_authentic
     def valid_password?(passwd)
 	Digest::MD5.hexdigest(passwd) == self.encrypted_passwd	
