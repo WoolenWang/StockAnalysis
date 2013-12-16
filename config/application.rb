44f8fb6697954370e3bd52dc3f68afbff0a521d9
woolen <349526042@qq.com>
2013-12-16 12:15:13 -0500

    fuck error

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,9 @@ module StockAnalysis
         # Version of your assets, change this if you want to expire all your assets
         config.assets.version = '1.0'
         config.gem 'authlogic'
+
+        # not to bootstrap your application during asset compilation in
+        config.assets.initialize_on_precompile = false
     end
 end
 
